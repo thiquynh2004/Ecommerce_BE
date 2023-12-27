3716908f371c622ba67476e01c95951503c21cb7
thiquynh2004 <ntq.nguyenquynh@gmail.com>
2023-12-27 10:38:12 +0700

    install compression

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
-const {default: helmet} = require('helmet')
+const {default: helmet} = require('helmet');
+const compression = require('compression');
 const app = express(); 
 
 //init middlewares
 app.use(morgan("dev"))
-
 app.use(helmet())
+app.use(compression())
 
 //init db
 
 //init routes
 
 app.get('/', (req, res, next) => {
-    return res.status(500).json({
+    return res.status(200).json({
         message: 'Welcome'
     })
 })
