1501aaf1d462b77bc324fd76637134ff7774ec00
thiquynh2004 <ntq.nguyenquynh@gmail.com>
2024-01-12 11:29:12 +0700

    update main branch

--- src/services/keyToken.service.js
+++ src/services/keyToken.service.js
@@ -1,12 +1,9 @@
 'use strict'
 
-
 const keytokenModel = require("../models/keytoken.model")
-
 class KeyTokenService {
     static createKeyToken = async ({userId, publicKey, privateKey}) => {
         try {
-            // const publicKeyString = publicKey.toString()
             const tokens = await keytokenModel.create({
                 user: userId,
                 publicKey: publicKey,
